21e8ad260c4022aa5bed25a877d02f2ab11e3e51
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-07-08 18:11:25 +0900

    fix: 🐛 クリエタブのタイトル抜けを修正

--- src/main/kotlin/dev/bluesheep/shepherd/registry/ShepherdItems.kt
+++ src/main/kotlin/dev/bluesheep/shepherd/registry/ShepherdItems.kt
@@ -3,6 +3,7 @@ package dev.bluesheep.shepherd.registry
 import dev.bluesheep.shepherd.Shepherd
 import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.Registries
+import net.minecraft.network.chat.Component
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.Item
@@ -32,6 +33,7 @@ object ShepherdItems {
     val CREATIVE_TAB_REGISTRY: DeferredRegister<CreativeModeTab> = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Shepherd.MODID)
     val TAB: DeferredHolder<CreativeModeTab, CreativeModeTab> = CREATIVE_TAB_REGISTRY.register("tab") { ->
         CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.shepherd"))
             .icon { SUPER_WHEAT.defaultInstance }
             .displayItems(REGISTRY.entries)
             .build()
